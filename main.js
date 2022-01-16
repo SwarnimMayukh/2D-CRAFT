d7f615b695f773616a47ba07e8ab8ac52c4d8505
Swarnim Mayukh <88070589+SwarnimMayukh@users.noreply.github.com>
2022-01-16 18:19:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '38'){
+        up();
+        console.log("up");
+
+    }
+}
+
+function up() {
+    window.location = "https://swarnimmayukh.github.io/2D-Craft-My-First-Try-./";
+}
